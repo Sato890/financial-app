09500a5698683d23957034bee304c4250a787359
sato <satoshighiba890@gmail.com>
2025-09-11 20:05:48 +0100

    feat: implement Transaction class and debt minimization
    The Transaction class was added to the model. A transaction contains who paid, the amount paid, the currency, a category, a date and time, and it specifies the way the debt is shared among the debtors through a list of DebtorShares. Each DebtorShare models how much a person owes.
    The Group class was updated to include a collection of Debts between the members of the groups.
    Whenever a transaction is added or removed, the debts are minimized through the "minimize_debts" service function.
    
    Unit tests using pytest.

--- domain/model.py
+++ domain/model.py
@@ -1,5 +1,9 @@
 from typing import Optional
 import uuid
+from dataclasses import dataclass, field
+import heapq
+from typing import List, Optional
+from datetime import date
 
 class Person:
     def __init__(self, name: str, id: Optional[str] = None):
@@ -19,16 +23,136 @@ class Person:
     
     def __str__(self):
         return self.name
-    
 
+
+@dataclass
+class DebtorShare:
+    debtor: "Person"
+    split_amount: float
+
+@dataclass(order=True)
+class Debt: 
+    debtor: Person = field(compare=False)
+    creditor: Person = field(compare=False)
+    amount: float
+
+class Transaction:
+    def __init__(self, who_paid: Person, amount: float, currency: str, debtor_shares: List[DebtorShare], category: str, date_time: date, id: Optional[str] = None):
+        self.who_paid = who_paid
+        self.amount = amount
+        self.currency = currency
+        self.debtor_shares = debtor_shares
+        self.category = category
+        self.date_time = date_time
+        self.id = id or uuid.uuid4()
+
+    def __repr__(self):
+        return (
+            f"Transaction("
+            f"who_paid={self.who_paid}, "
+            f"amount={self.amount}, "
+            f"currency={self.currency}, "
+            f"debtor_shares={self.debtor_shares}, "
+            f"category={self.category}, "
+            f"date_time={self.date_time})"
+        )
+        
+    def __eq__(self, other):
+        return isinstance(other, Transaction) and self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+    
 class Group:
     def __init__(self, name: str, id: Optional[str] = None):
         self.id = id or uuid.uuid4()
         self.name = name
-        self.persons = set()            
+        self.persons = set()
+        self.transactions = []
+        self.debts = []            
 
     def add_person(self, person: Person): 
         self.persons.add(person)
 
     def remove_person(self, person: Person): 
         self.persons.remove(person)
+
+    def add_transaction(self, transaction: Transaction):
+        self.transactions.append(transaction)
+        self._recalculate_debts()
+
+    def remove_transaction(self, transaction: Transaction):
+        self.transactions.remove(transaction)
+        self._recalculate_debts()
+    
+    def _recalculate_debts(self):
+        raw_debts = []
+        for t in self.transactions:
+            for ds in t.debtor_shares:
+                if ds.debtor != t.who_paid:
+                    amount = -ds.split_amount
+                    raw_debts.append(Debt(debtor=ds.debtor, creditor=t.who_paid, amount=amount))
+        self.debts = minimize_debts(raw_debts)
+
+    @property
+    def total_share(self) -> dict:
+        shares = {}
+        for t in self.transactions:
+            for ds in t.debtor_shares:
+                shares[ds.debtor] = shares.get(ds.debtor, 0) + ds.split_amount
+        return shares
+
+    def add_person(self, person: Person): 
+        self.persons.add(person)
+
+    def remove_person(self, person: Person): 
+        self.persons.remove(person)
+
+    def _add_debt(self, debt: Debt):
+        self.debts.append(debt)
+
+def get_net_owed_balances(debts: List[Debt]) -> dict:
+        owed_balances = {}
+
+        for debt in debts:
+            owed_balances[debt.debtor] = owed_balances.get(debt.debtor, 0) - debt.amount
+            owed_balances[debt.creditor] = owed_balances.get(debt.creditor, 0) + debt.amount
+
+        return owed_balances
+
+
+def minimize_debts(debts: List[Debt]) -> List[Debt]:
+    
+    net_debts = get_net_owed_balances(debts)
+    negative_debts = []
+    positive_debts = []
+
+    index_person = 0
+    for person, amount in net_debts.items():
+        if amount < 0:
+            heapq.heappush(negative_debts, (amount, index_person, person))
+        elif amount > 0:
+            heapq.heappush(positive_debts, (amount, index_person, person))
+        
+        index_person += 1
+    
+    settlements = [] 
+
+    while negative_debts and positive_debts:
+
+        neg_amount, index_debtor, debtor = heapq.heappop(negative_debts)
+        pos_amount, index_creditor, creditor = heapq.heappop(positive_debts)
+
+        settlement_amount = min(-neg_amount, pos_amount)
+
+        settlements.append(Debt(debtor, creditor, settlement_amount))
+
+        new_neg_amount = neg_amount + settlement_amount
+        new_pos_amount = pos_amount - settlement_amount
+
+        if new_neg_amount < 0:
+            heapq.heappush(negative_debts, (new_neg_amount, index_debtor, debtor))
+        if new_pos_amount > 0:
+            heapq.heappush(positive_debts, (new_pos_amount, index_creditor, creditor))
+
+    return [d for d in settlements if abs(d.amount) > 0]
---
+++ tests/unit/test_transactions.py
@@ -0,0 +1,210 @@
+from domain.model import *
+from datetime import date
+
+
+def test_adding_transaction_updates_total_share_by_person():
+    group = Group("group1", "EUR")
+
+    person1 = Person("luigi")
+    person2 = Person("mario")
+
+    group.add_person(person1)
+    group.add_person(person2)
+    
+    share1 = DebtorShare(person1, 100)
+    share2 = DebtorShare(person2, 100)
+    
+    share3 = DebtorShare(person2, 200)
+
+    transaction = Transaction(person1, 200, "EUR", [share1, share2], "Trip", date.today())
+
+    group.add_transaction(transaction)
+
+    assert group.total_share[person1] == 100
+    assert group.total_share[person2] == 100
+    
+    transaction2 = Transaction(person1, 200, "EUR", [share3], "Trip", date.today())
+    group.add_transaction(transaction2)
+    
+    assert group.total_share[person2] == 300
+
+def test_adding_transaction_updates_debt_quantities():
+    group = Group("group1", "EUR")
+
+    person1 = Person("luigi")
+    person2 = Person("mario")
+    
+    share1 = DebtorShare(person1, 100)
+    share2 = DebtorShare(person2, 100)
+
+    group.add_person(person1)
+    group.add_person(person2)
+
+    transaction = Transaction(person1, 200, "EUR", [share1, share2], "Trip", date.today())
+
+    group.add_transaction(transaction)
+
+    debt1 = Debt(person2, person1, 100)
+
+    assert debt1 in group.debts 
+
+def test_removing_person_when_debt_exists():
+    group = Group("group1", "EUR")
+
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+
+    group.add_person(person1)
+    group.add_person(person2)
+
+    share1 = DebtorShare(person1, 100)
+    share2 = DebtorShare(person2, 100)
+    share3 = DebtorShare(person2, 200)
+    
+    transaction = Transaction(person1, 200, "EUR", [share1, share2], "Trip", date.today())
+
+    group.add_transaction(transaction)
+
+    debt1 = Debt(person2, person1, 100)
+
+    group.remove_person(person2)
+    group.remove_person(person1)
+
+    assert debt1 in group.debts   
+
+def test_calculate_net_debts():
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+    person3 = Person("Peach")
+
+    debt1 = Debt(person1, person2, 50)
+    debt2 = Debt(person2, person3, 30)
+    debt3 = Debt(person1, person3, 20)
+
+    debts = [debt1, debt2, debt3]
+
+    net_balances = get_net_owed_balances(debts)
+
+    assert net_balances[person1] == -70
+    assert net_balances[person2] == +20
+    assert net_balances[person3] == +50
+
+def test_minimize_debts():
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+    person3 = Person("Peach")
+
+    debt1 = Debt(person1, person2, 50)
+    debt2 = Debt(person2, person3, 30)
+    debt3 = Debt(person1, person3, 20)
+
+    debts = [debt1, debt2, debt3]  
+
+    minimized_debts = minimize_debts(debts)
+
+    assert len(minimized_debts) == 2
+    assert Debt(person1, person2, 20) in minimized_debts
+    assert Debt(person1, person3, 50) in minimized_debts
+
+def test_minimize_debts_no_debts():
+    debts = []
+    minimized_debts = minimize_debts(debts)
+    assert minimized_debts == []
+
+def test_minimize_debts_all_settled():
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+
+    debt1 = Debt(person1, person2, 50)
+    debt2 = Debt(person2, person1, 50)
+
+    debts = [debt1, debt2]
+
+    minimized_debts = minimize_debts(debts)
+
+    assert minimized_debts == []
+
+
+def test_minimize_debts_single_person():
+    person1 = Person("Luigi")
+
+    debts = []
+
+    minimized_debts = minimize_debts(debts)
+
+    assert minimized_debts == []
+
+
+def test_debt_greater_than_other_debt():
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+
+    debt1 = Debt(person1, person2, 100)
+    debt2 = Debt(person2, person1, 30)
+
+    assert debt1 > debt2
+
+def test_add_multiple_transactions_and_check_debts():
+    group = Group("group1", "EUR")
+
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+
+    group.add_person(person1)
+    group.add_person(person2)
+    
+    share1 = DebtorShare(person1, 150)
+    share2 = DebtorShare(person2, 150)
+    share3 = DebtorShare(person1, 100)
+    share4 = DebtorShare(person2, 100)
+    share5 = DebtorShare(person1, 50)
+    share6 = DebtorShare(person2, 50)
+    
+    transaction1 = Transaction(person1, 300, "EUR", [share1, share2], "Spesa1", date.today())
+    transaction2 = Transaction(person2, 200, "EUR", [share3, share4], "Spesa2", date.today())
+    transaction3 = Transaction(person1, 100, "EUR", [share5, share6], "Spesa3", date.today())
+
+    group.add_transaction(transaction1)
+    group.add_transaction(transaction2)
+    group.add_transaction(transaction3)
+
+    net_balances = get_net_owed_balances(group.debts)
+
+    assert net_balances[person1] == -100
+    assert net_balances[person2] == +100
+
+def test_remove_transaction_and_check_debts():
+    group = Group("group1", "EUR")
+
+    person1 = Person("Luigi")
+    person2 = Person("Mario")
+
+    group.add_person(person1)
+    group.add_person(person2)
+
+    share1 = DebtorShare(person1, 150)
+    share2 = DebtorShare(person2, 150)
+    share3 = DebtorShare(person1, 100)
+    share4 = DebtorShare(person2, 100)
+    share5 = DebtorShare(person1, 500)
+    share6 = DebtorShare(person2, 500)
+    
+    transaction1 = Transaction(person1, 300, "EUR", [share1, share2], "Spesa1", date.today())
+    transaction2 = Transaction(person2, 200, "EUR", [share3, share4], "Spesa2", date.today())
+    transaction3 = Transaction(person1, 1000, "EUR", [share5, share6], "Spesa3", date.today())
+
+    group.add_transaction(transaction1)
+    group.add_transaction(transaction2)
+    group.add_transaction(transaction3)
+
+    group.remove_transaction(transaction3)
+
+    net_balances = get_net_owed_balances(group.debts)
+
+    assert net_balances[person1] == -50
+    assert net_balances[person2] == +50
+    
+    assert group.total_share[person1] == 250
+
+
+    
